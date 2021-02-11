efe8b55b04c53e1a2bd0399e7fd0cc4687ac8d4f
rahul-hunterdigitaldev <rahulh@hunter.digital>
2021-02-11 10:36:02 +0530

    The logic now works

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const moment = require("moment");
 
-let leaseStart = "2021-03-01";
-let leaseEnd = "2021-05-01";
-const monthlyRentAmount = 10000;
+let leaseStart = "2021-04-10";
+let leaseEnd = "2022-03-15";
+const monthlyRentAmount = 100000;
 const YYYYMMDD = "YYYY-MM-DD";
 const MMDDYYYY = "MM/DD/YYYY";
 const invalidDate = "INVALID DATE";
@@ -18,20 +18,19 @@ const getMonthlyRent = () => {
         leaseEnd = moment(leaseEnd, endDateFormat);
         leaseEnd = moment(new Date(leaseEnd).setDate(new Date(leaseEnd).getDate() - 1));
     };
-    const lastDayOfStartMonth = moment(new Date(leaseStart).setDate(leaseStart.daysInMonth()));
-    const duration = moment.duration(leaseEnd.diff(leaseStart)).asDays();
-    const durationMonth = moment.duration(lastDayOfStartMonth.diff(moment(leaseStart))).asDays();
-    if(durationMonth >= duration) {
-        responseData.push({ periodStart: leaseStart.format('MM/DD/YYYY'), periodEnd: leaseEnd.format('MM/DD/YYYY'), amount: (monthlyRentAmount*duration/durationMonth) })
-    } else {
-        const durationInMonth = moment.duration(leaseEnd.diff(leaseStart)).asMonths();
-        for(let index = 0; index < Math.ceil(durationInMonth); index += 1) {
-            const startDate = moment(new Date(leaseStart).setMonth(new Date(leaseStart).getMonth()+index));
-            const endDate = moment(new Date(startDate).setDate(startDate.daysInMonth()+1));
-            const days =  moment.duration(endDate.diff(moment(startDate))).asDays();
-            const daysInMonth = startDate.daysInMonth();
-            responseData.push({ periodStart: startDate.format('MM/DD/YYYY'), periodEnd: endDate.format('MM/DD/YYYY'), amount: (monthlyRentAmount*days/daysInMonth) });
+    let duration = moment.duration(leaseEnd.diff(leaseStart)).asDays();
+    const durationInMonth = moment.duration(leaseEnd.diff(leaseStart)).asMonths();
+    for(let index = 0; index <= Math.floor(durationInMonth); index += 1) {
+        const startDate = moment(new Date(new Date(leaseStart).setMonth(new Date(leaseStart).getMonth()+index)).setDate(index > 0 ? 01 : new Date(leaseStart).getDate()));
+        const isFullMonth = duration >= (startDate.daysInMonth() - new Date(startDate).getDate() + 1) ? true : false;
+        if(isFullMonth) {
+            duration -= (startDate.daysInMonth() - new Date(startDate).getDate() + 1);
         }
+        const endDate = moment(new Date(startDate).setDate(isFullMonth ? startDate.daysInMonth()+1 : new Date(startDate).getDate()+duration+1));
+        const days =  moment.duration(endDate.diff(moment(startDate))).asDays();
+        const daysInMonth = startDate.daysInMonth();
+        if(days)
+        responseData.push({ periodStart: startDate.format('MM/DD/YYYY'), periodEnd: endDate.format('MM/DD/YYYY'), amount: Math.round(monthlyRentAmount*days/daysInMonth) });
     }
     console.log(responseData);
 }
